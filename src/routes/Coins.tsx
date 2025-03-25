0cb1e0655d361a3ceece7403a485cf617acb1608
gyeol-mi <gyeolmi9876@gmail.com>
2025-03-25 14:52:06 +0900

    #5.1_Styles: GlobalStyle

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,12 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+
 function Coins() {
   console.log("ddd");
-  return <h1>Coins</h1>;
+  return <Title>Coins</Title>;
 }
 
 export default Coins;
